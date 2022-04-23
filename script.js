92023d09bedc7c160c90d4d04d81389f25663415
balachandarkec <balachandar.s@niit.com>
2022-04-23 20:23:01 +0530

    events added

--- script.js
+++ script.js
@@ -16,8 +16,11 @@ let addItems=function(){
 };
 
 
-// Event Registration
+// Event Registration for pre loaded elements
 btn.addEventListener('click',addItems);
+
+
+// Add event listener for dynamic elements
 
 document.addEventListener('click',function(e){
   if(e.target.className=='item') {
@@ -32,4 +35,77 @@ document.addEventListener('click',function(e){
 });
 
 
+// Demo Of Keypress Event 
+input_ele.addEventListener('keypress',(event)=>{
+console.log("typing......");
+});
+
+/* Demo Of Scroll Event
+
+window.addEventListener('scroll',(e)=>{
+ console.log("scrolling,,,,,,,,,,,,,,");
+});
+*/
+window.onscroll=(e)=>{
+  console.log("scrolling,,,,,,,,,,,,,,");};
+ 
+
+/* On Page Load Events
+   1.DOMContentLoaded
+   2.load
+   3. beforUnload
+   4. unload
+  
+   */
+
+document.addEventListener('DOMContentLoaded',()=>{
+window.alert("DOMContentLoaded");
+});
+
+document.addEventListener('load',()=>{
+  console.log("Load event-page completly loaded!");
+});
+document.addEventListener('beforeunload',(event)=>{
+
+  window.alert("before unload");
+  event.preventDefault();
+  event.returnValue="";
+});
+document.addEventListener('unload',()=>{
+   console.log("unloaded");
+});
+
+
+/* OnLoad Event
+1 .load - Image Onloadand Script on load
+
+Mouse Events :
+ 1. mousedown
+ 2. mouseup
+ 3. click
+ 4. dblclick
+ 5. mousremove
+ 6. mouseover
+ 7.mouseremove/mouseout
+ 8. mouseenter
+ 9. mouseleave
+
+ keyboard Events :
+
+ 1. keydown
+ 2. keyup
+ 3. keypress
+
+ Scroll Events
+
+ focus events - blur  focus
+
+ haschange - event
+
+
+ 
+
+
+
+*/
 
